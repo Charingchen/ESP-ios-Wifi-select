f9fc37df346a760627e5dd3b8673ded294aed165
Charing chen <charinghaxen@gmail.com>
2018-11-30 10:25:44 -0800

    Adding socket sending scripts, wifi scan works

--- ESP ios Wifi select/MainNavController.swift
+++ ESP ios Wifi select/MainNavController.swift
@@ -4,7 +4,8 @@
 //
 //  Created by Hanxiang Chen on 2018-10-31.
 //  Copyright © 2018 Haxen Tech. All rights reserved.
-//
+// Wishlist Nov 8:
+// implement dismess or back function for password selection screen, or go back to first controller to report connection extabilished 
 
 import UIKit
 
@@ -19,21 +20,26 @@ class MainNavController: UIViewController {
     
     @IBOutlet weak var initiConnButton: UIButton!
     @IBOutlet weak var wifiScanButton: UIButton!
+    @IBOutlet weak var instruction: UITextView!
     
     @IBAction func connectDidTap(_ sender: Any) {
-        print("initi socket connection")
-        print("socket connected")
+        UIApplication.shared.open(URL(string:"App-Prefs:root=WIFI")!)
         
         //hide initiConnButton and show WifiScan button
         initiConnButton.isHidden = true
         wifiScanButton.isHidden = false
+        instruction.text = "Click the button below to commond esp32 to start Wifi scan"
     
     }
     
     @IBAction func wifiScanDidTap(_ sender: Any) {
         print("wifi scan started")
+        let esp32 = socketComm()
+        let tempssidstruct = esp32.wifiScan()
+        print("tempssidstruct : \(tempssidstruct)")
         //prepare sample ssid and wifi image
-        let listTosend = createArray()
+        //let listTosend = createArray()
+        let listTosend = processWifiStruc(wifilist: tempssidstruct)
         //Set up wifiListScreenViewController
         let wifiVC = storyboard?.instantiateViewController(withIdentifier: "wifilistview") as! WifiListScreenViewController
         //need this line to initial wifidelegate and point to wifilistScreenController
@@ -64,6 +70,26 @@ class MainNavController: UIViewController {
         tempssid.append (ssid1)
         tempssid.append(ssid2)
         tempssid.append(ssid3)
+        return tempssid
+    }
+    
+    func processWifiStruc (wifilist:[WIFIinfo]) -> [Ssid]{
+        let tempstruc = wifilist
+        var tempssid: [Ssid] = []
+        var ssid_temp = Ssid(image: #imageLiteral(resourceName: "one_bar"), id: "nil")
+        for stn in tempstruc {
+            let signal_strength = Int(stn.RSSI)!
+            if signal_strength >  -60{
+                 ssid_temp = Ssid(image: #imageLiteral(resourceName: "wifi_3bar") , id: stn.SSID)
+            }
+            else if signal_strength <= -60 && signal_strength > -80 {
+                 ssid_temp = Ssid(image: #imageLiteral(resourceName: "wifi_2bar") , id: stn.SSID)
+            }
+            else{
+                 ssid_temp = Ssid(image: #imageLiteral(resourceName: "one_bar") , id: stn.SSID)
+            }
+            tempssid.append(ssid_temp)
+        }
         return tempssid
     }
 
---
+++ ESP ios Wifi select/SocketCom.swift
@@ -0,0 +1,164 @@
+//
+//  SocketCom.swift
+//  ESP ios Wifi select
+//
+//  Created by Hanxiang Chen on 2018-11-20.
+//  Copyright © 2018 Haxen Tech. All rights reserved.
+//
+
+import Foundation
+import SwiftSocket
+
+struct WIFIinfo{
+    let SSID: String
+    let RSSI: String
+    let AUTH: String
+}
+
+class socketComm {
+    let sendGood = "CMD received!"
+    let sendBad = "Got NO CMD!"
+    let startScan = "c1"
+    let enableTx = "c2"
+    let client = TCPClient(address: "192.168.1.1", port: 100)
+    
+    public func wifiScan() -> [WIFIinfo]{
+        //var dataRev = sendCmd(cmd: startScan)
+        //print(dataRev)
+        //dataRev = sendCmd(cmd: enableTx)
+        //print("dataRecv:\(dataRev)")
+        let dataRecv: String = "3#SSID:fortinet. RSSI:-40. Authmode: unknown.\nSSID:wiiwl. RSSI:-68. Authmode: WIFI_AUTH_WPA2_PSK.\nSSID:kkksi. RSSI:-78. Authmode: WIFI_AUTH_WPA2_PSK.\n"
+        let wifiStruc = prepareSSIDlist(dataRecv: dataRecv)
+        return wifiStruc
+    }
+    
+    private func sendCmd (cmd: String) -> String{
+        //assign it to nil for debugging purpose
+        var dataread: String = "nil"
+        switch client.connect(timeout: 10) {
+            // first case of success + fail is for connection establishment
+        case .success:
+            print("Client Connect Succussful")
+            switch client.send(string: cmd ) {
+                //This determine if the sending is succussful
+            case .success:
+                print("Send request succussful")
+                // Reading socket package of 1024 * 10, if there no data return the function right away. This is a less gracful way to achieve ACK
+                guard let data = client.read(1024*10, timeout: 5) else { return "No data read" }
+                
+                if var response = String(bytes: data, encoding: .utf8) {
+                    print("Response: \(response)")
+                    dataread = response
+                    //sendBad are sending from esp32, to indicate no cmd recieved.
+                    if response == sendBad{
+                        // if no ACK from esp32, try to spam the esp32 with the cmd until it ack
+                        repeat{
+                            print("Re-sending the command")
+                            switch client.connect(timeout: 10) {
+                            case .success:
+                                print("Client Connect Succussful")
+                                switch client.send(string: cmd ) {
+                                case .success:
+                                    print("Send request succussful")
+                                    guard let data = client.read(1024*10, timeout: 5) else { return "No data read" }
+                                    response = String(bytes: data, encoding: .utf8)!
+                                case .failure(let error):
+                                    print("send fail, error: \(error)")
+                                    client.close()
+                                }
+                            case .failure(let error):
+                                print("send fail, error: \(error)")
+                            }
+                        } while response == sendBad
+                    }
+                }
+                return dataread
+            case .failure(let error):
+                print("send fail, error: \(error)")
+                client.close()
+            }
+        case .failure(let error):
+            print("Client connect failure, error: \(error)")
+        }
+        return "nil"
+    }
+    
+    private func prepareSSIDlist (dataRecv: String) -> [WIFIinfo]{
+        var ssidList = [String]()
+        var wifilist = [WIFIinfo]()
+        var rawdata: String = dataRecv
+        let hashIndex = rawdata.index(before:rawdata.index(of:"#") ?? rawdata.endIndex)
+        if let staNum = Int(String(rawdata[...hashIndex])){
+            print("sta number rec:\(staNum)")
+            rawdata.removeSubrange(rawdata.startIndex...rawdata.index(after: hashIndex))
+            
+            for i in 0..<staNum {
+                let location = rawdata.index(after:rawdata.index(of:"\n") ?? rawdata.endIndex)
+                let string1 = String(rawdata[..<location])
+                ssidList.append(string1.replacingOccurrences(of: "\n", with: " "))
+                rawdata.removeSubrange(rawdata.startIndex..<location)
+                print("partial substring1:\(ssidList[i])")
+                
+            }
+            for info in ssidList{
+                var tempString = info
+                var colonlocation = tempString.index(after:tempString.index(of:":") ?? tempString.endIndex)
+                var spacelocation = tempString.index(before:tempString.index(of:".") ?? tempString.endIndex)
+                let ssid = String(tempString[colonlocation...spacelocation])
+                tempString.removeSubrange(tempString.startIndex...tempString.index(after: spacelocation))
+                colonlocation = tempString.index(after:tempString.index(of:":") ?? tempString.endIndex)
+                spacelocation = tempString.index(before:tempString.index(of:".") ?? tempString.endIndex)
+                let rssi = String(tempString[colonlocation...spacelocation])
+                tempString.removeSubrange(tempString.startIndex...tempString.index(after: spacelocation))
+                colonlocation = tempString.index(after:tempString.index(of:":") ?? tempString.endIndex)
+                spacelocation = tempString.index(before:tempString.index(of:".") ?? tempString.endIndex)
+                let auth = String(tempString[colonlocation...spacelocation])
+                let newinfo = WIFIinfo(SSID: ssid, RSSI: rssi, AUTH: auth)
+                wifilist.append(newinfo)
+            }
+        }
+        print("ssidList: \(ssidList)")
+        print("wifilist: \(wifilist)")
+        return wifilist
+    }
+}
+
+extension String {
+    var length: Int {
+        return self.count
+    }
+    
+    func indices(of occurrence: String) -> [Int] {
+        var indices = [Int]()
+        var position = startIndex
+        while let range = range(of: occurrence, range: position..<endIndex) {
+            let i = distance(from: startIndex,
+                             to: range.lowerBound)
+            indices.append(i)
+            let offset = occurrence.distance(from: occurrence.startIndex,
+                                             to: occurrence.endIndex) - 1
+            guard let after = index(range.lowerBound,
+                                    offsetBy: offset,
+                                    limitedBy: endIndex) else {
+                                        break
+            }
+            position = index(after: after)
+        }
+        return indices
+    }
+    subscript (r: Range<Int>) -> String {
+        let range = Range(uncheckedBounds: (lower: max(0, min(length, r.lowerBound)),
+                                            upper: min(length, max(0, r.upperBound))))
+        let start = index(startIndex, offsetBy: range.lowerBound)
+        let end = index(start, offsetBy: range.upperBound - range.lowerBound)
+        return String(self[start ..< end])
+    }
+    
+    func index(at offset: Int) -> String.Index {
+        return index(startIndex, offsetBy: offset)
+    }
+    //    subscript (inttoindex: Int) -> String.Index {
+    //        let charIndex = self.index(self.startIndex, offsetBy: inttoindex)
+    //        return charIndex
+    //    }
+}
